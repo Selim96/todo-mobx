3e6cacde934d1e576a8503e1973db33f1ac0c3c3
Selim <93911103+Selim96@users.noreply.github.com>
2024-03-06 18:06:48 +0200

    add

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import s from "./Container.module.scss";
+import React from "react"
+import s from "./Container.module.scss"
 
 interface IProps {
   children?: React.ReactNode
 }
 
 const Container: React.FC< IProps> = ({children}) => {
-  return <div className={s.container}>{children}</div>;
-};
+  return <div className={s.container}>{children}</div>
+}
 
-export default Container;
+export default Container
